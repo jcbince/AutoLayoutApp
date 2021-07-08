1ea72cf940b8e04df061d71687481c104c149654
Jc Bince <email@jcbince.ca>
2021-07-08 13:26:35 -0700

    creation of AutoLayoutApp files and git folder + clone

---
+++ AutoLayoutApp/AutoLayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutApp
+//
+//  Created by user198555 on 7/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
